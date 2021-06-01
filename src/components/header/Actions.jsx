37726085c23d46573b78561997a26e73db6002ad
DavidG <datog309@gmail.com>
2021-06-01 21:04:22 +0400

    cart link

--- src/components/header/Actions.jsx
+++ src/components/header/Actions.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import CurrencySelector from "./CurrencySelector";
-import Cart from "./Cart";
+import CartIcon from "./CartIcon";
 
 class Actions extends React.Component {
 
@@ -9,7 +9,7 @@ class Actions extends React.Component {
         return(
             <div id="actions">
                 <CurrencySelector />
-                <Cart />
+                <CartIcon />
             </div>
         );
     }
