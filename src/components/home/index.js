7aa48f60d48f4c8c2f31cb235c4313a91349b379
DavidG <datog309@gmail.com>
2021-06-02 17:14:25 +0400

    root styles

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,37 +2,8 @@ import React, { Component } from 'react';
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
+import { SLink, Container, Item } from "./home.styles"
 
-const Container = styled.div`
-    display: flex;
-    width: 60%;
-    height: 100%;
-    margin: 1rem 2rem;
-    margin: auto;
-    justify-content: space-between;
-`;
-
-const Item = styled.div`
-    width: 300px;
-    height: 30vh;
-    font-size: 1.5vw;
-    font-weight: 600;
-    text-transform: capitalize;
-    background-color: #F0F8FF;
-    padding: 1.3rem 2.3rem;
-    color: #1D1F22;
-    &:hover {
-        color: #5ECE7B
-    }
-`;
-
-const SLink = styled(Link)`
-    text-decoration: none;
-    color: #1D1F22;
-    &:hover {
-        color: #5ECE7B
-    }
-`;
 
 export default class Home extends Component {
     render() {
