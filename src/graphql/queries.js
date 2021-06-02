59823376957c55e9211d5bc67ed4300504bdd853
DavidG <datog309@gmail.com>
2021-06-02 18:18:23 +0400

    removed unwanted fields from getItems query

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -4,13 +4,10 @@ import { gql } from "@apollo/client";
 export const getItems = gql`
     query getItems($name: String!){
         category(input: { title: $name }) {
-            name
             products {
                 name
                 inStock
                 gallery
-                description
-                category
                 prices {
                     currency
                     amount
