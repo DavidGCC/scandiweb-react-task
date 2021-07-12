f2e38080837964f01c48f87ed7cfda2a807a9ac2
DavidG <datog309@gmail.com>
2021-07-12 21:32:07 +0400

    added add to cart on plp

--- src/components/listings/Item.jsx
+++ src/components/listings/Item.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
 
 import { StoreContext } from "context/Context";
 import {
@@ -14,23 +15,59 @@ import {
     ItemImageContainer
 } from "./listings.styles";
 
+import AddToCart from "./AddToCart";
 import Price from "../shared/Price";
 
 class Item extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = { isHovering: false };
+        this.handleMouseEnter = this.handleMouseEnter.bind(this);
+        this.handleMouseleave = this.handleMouseleave.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleMouseEnter() {
+        this.setState({ isHovering: true });
+    }
+
+    handleMouseleave() {
+        this.setState({ isHovering: false });
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+        const { item, history } = this.props;
+        if (item.attributes.length === 0) {
+            this.context.addToCart(item);
+        } else {
+            history.push(`/product/${item.name}`);
+        }
+    }
+
+
+
     render() {
+        const { isHovering } = this.state;
+        const { item } = this.props;
         return (
-            <ItemContainer>
+            <ItemContainer onMouseOver={this.handleMouseEnter} onMouseLeave={this.handleMouseleave}>
                 <ItemTop>
                     <ItemImageContainer>
                         <ItemImage
                             alt="item preview"
-                            src={this.props.item.gallery[0]}
+                            src={item.gallery[0]}
                             width="354px"
                             height="330px"
                         />
                     </ItemImageContainer>
                     {
-                        !this.props.item.inStock && (
+                        isHovering && item.inStock && (
+                            <AddToCart onClick={this.handleClick} />
+                        )
+                    }
+                    {
+                        !item.inStock && (
                             <OutOfStockOverlay>
                                 <OutOfStockText>out of stock</OutOfStockText>
                             </OutOfStockOverlay>
@@ -38,11 +75,11 @@ class Item extends React.PureComponent {
                     }
                 </ItemTop>
                 <ItemBot>
-                    <ItemName inStock={this.props.item.inStock}>{this.props.item.name}</ItemName>
-                    <Price prices={this.props.item.prices}>
+                    <ItemName inStock={item.inStock}>{item.name}</ItemName>
+                    <Price prices={item.prices}>
                         {
                             (symbol, amount) => (
-                                <ItemPrice inStock={this.props.item.inStock}>{`${symbol}${amount}`}</ItemPrice>
+                                <ItemPrice inStock={item.inStock}>{`${symbol}${amount}`}</ItemPrice>
                             )
                         }
                     </Price>
@@ -54,7 +91,8 @@ class Item extends React.PureComponent {
 Item.contextType = StoreContext;
 
 Item.propTypes = {
-    item: PropTypes.object
+    item: PropTypes.object,
+    history: PropTypes.object
 };
 
-export default Item;
+export default withRouter(Item);
